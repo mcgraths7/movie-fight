4796ebb8e389518de652f44485598eca5dfb6589
mcgraths7 <mcgrath.steven7@gmail.com>
2020-08-22 14:49:50 -0400

    refactored html generation to use innerHTML

--- index.js
+++ index.js
@@ -1,58 +1,57 @@
 const generateAutocompleteWidget = (id) => {
   const autocompleteDiv = document.getElementById(id);
-
-  const movieInput = document.createElement('input');
-  movieInput.classList.add('movieSearchField');
-
-  const dropdown = document.createElement('div');
-  dropdown.classList.add('dropdown-menu');
-
-  const contentDiv = document.createElement('div');
-  contentDiv.classList.add('dropdown-content');
-
-  autocompleteDiv.appendChild(movieInput);
-  autocompleteDiv.appendChild(dropdown);
-  dropdown.appendChild(contentDiv);
+  autocompleteDiv.innerHTML = `
+    <label>
+      <strong>Search for a Movie</strong>
+      <input class='input' />
+    </label>
+    <div class="dropdown">
+      <div class='dropdown-menu'>
+        <div class='dropdown-content results'></div>
+      </div>
+    </div>
+  `;
 };
 
-const onInput = debounce(async (e) => {
-  const dropdown = document.querySelector('.dropdown-menu');
-  dropdown.style.display = 'block';
-  const contentDiv = document.querySelector('.dropdown-content');
-  const movies = await fetchSeveralMovies(e.target.value);
-  for (let movie of movies) {
-    createMovieListItem(movie, contentDiv);
-  }
-});
+generateAutocompleteWidget('firstAutocomplete');
+const dropdown = document.querySelector('.dropdown');
+const movieInput = document.querySelector('input');
+const resultsWrapper = document.querySelector('.results');
 
-const onBlur = () => {
-  const dropdown = document.querySelector('.dropdown-menu');
-  dropdown.style.display = 'none';
-};
-
-const onFocus = () => {
-  const dropdown = document.querySelector('.dropdown-menu');
-  const dropdownContent = document.querySelector('.dropdown-content');
-  if (dropdownContent.childElementCount > 0) {
-    dropdown.style.display = 'block';
-  }
-};
-
-const createMovieListItem = (movie, contentDiv) => {
+const createMovieListItem = (movie, resultsWrapper) => {
   const movieItem = document.createElement('a');
   movieItem.classList.add('dropdown-item');
   movieItem.innerHTML = `
         <img src="${movie.Poster}" width="50px" />
         <p>${movie.Title} (${movie.Year})</p>
       `;
-  contentDiv.appendChild(movieItem);
+  resultsWrapper.appendChild(movieItem);
 };
 
-generateAutocompleteWidget('firstAutocomplete');
+const onInput = debounce(async (e) => {
+  dropdown.style.display = 'block';
+  const movies = await fetchSeveralMovies(e.target.value);
+  for (let movie of movies) {
+    createMovieListItem(movie, resultsWrapper);
+  }
+  dropdown.classList.add('is-active');
+});
+
+const onBlur = () => {
+  dropdown.classList.remove('is-active');
+};
+
+const onFocus = () => {
+  const dropdownContent = document.querySelector('.dropdown-content');
+  if (dropdownContent.childElementCount > 0) {
+    dropdown.classList.add('is-active');
+  }
+};
+
 // generateAutocompleteWidget('secondAutocomplete');
 
 // movieInputs = document.querySelectorAll('input');
-const movieInput = document.querySelector('input');
+
 // console.log(movieInputs);
 movieInput.addEventListener('input', onInput);
 movieInput.addEventListener('blur', onBlur);
