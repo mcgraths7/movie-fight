0727b86e1e3dd89eb461ed40922f4b50444a9304
mcgraths7 <mcgrath.steven7@gmail.com>
2020-08-24 12:00:25 -0400

    added styling when comparing two movies - adds and removes bulma notification classes when one value is greater than the other, or when there's no comparison (an N/A value) or when there is a tie

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const autoCompleteConfig = {
       params: {
         apikey: '219263fc',
         s: searchTerm,
+        type: 'movie',
       },
     });
 
@@ -27,23 +28,25 @@ const autoCompleteConfig = {
 };
 
 createAutoComplete({
-  root: document.querySelector('.left-autocomplete'),
+  root: document.querySelector('#left-autocomplete'),
   onOptionSelect(movie) {
-    const summary = document.querySelector('.left-summary');
-    onMovieSelect(movie, summary);
+    const summary = document.querySelector('#left-summary');
+    onMovieSelect(movie, 'left', summary);
   },
   ...autoCompleteConfig,
 });
 createAutoComplete({
-  root: document.querySelector('.right-autocomplete'),
+  root: document.querySelector('#right-autocomplete'),
   onOptionSelect(movie) {
-    const summary = document.querySelector('.right-summary');
-    onMovieSelect(movie, summary);
+    const summary = document.querySelector('#right-summary');
+    onMovieSelect(movie, 'right', summary);
   },
   ...autoCompleteConfig,
 });
 
-const onMovieSelect = async (movie, summaryElement) => {
+let leftResults, rightResults;
+
+const onMovieSelect = async (movie, movieSide, summaryElement) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '219263fc',
@@ -52,12 +55,21 @@ const onMovieSelect = async (movie, summaryElement) => {
   });
   const tutorial = document.querySelector('.tutorial');
   if (tutorial) {
-    tutorial.remove();
+    tutorial.classList.add('is-hidden');
   }
   summaryElement.innerHTML = movieTemplate(response.data);
+  if (movieSide === 'left') {
+    leftResults = parseMovieStats(response.data);
+  } else if (movieSide === 'right') {
+    rightResults = parseMovieStats(response.data);
+  }
+  if (leftResults && rightResults) {
+    compareMovies(leftResults, rightResults);
+  }
 };
 
 const movieTemplate = (movieDetail) => {
+  const movieStats = parseMovieStats(movieDetail);
   return `
     <article class="media">
       <figure class="media-left">
@@ -73,25 +85,91 @@ const movieTemplate = (movieDetail) => {
         </div>
       </div>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${movieStats.awardScore} class="notification is-primary">
       <p class="title">${movieDetail.Awards}</p>
       <p class="subtitle">Awards</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${movieStats.boxOfficeDollars} class="notification is-primary">
       <p class="title">${movieDetail.BoxOffice}</p>
       <p class="subtitle">Box Office</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${movieStats.metascore} class="notification is-primary">
       <p class="title">${movieDetail.Metascore}</p>
       <p class="subtitle">Metascore</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${movieStats.imdbRating} class="notification is-primary">
       <p class="title">${movieDetail.imdbRating}</p>
       <p class="subtitle">IMDB Rating</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${movieStats.imdbVotes} class="notification is-primary">
       <p class="title">${movieDetail.imdbVotes}</p>
       <p class="subtitle">IMDB Votes</p>
     </article>
   `;
+};
+
+const parseMovieStats = (movie) => {
+  const boxOfficeDollars = movie.BoxOffice.replace(/\$/g, '').replace(/,/g, '');
+  const metascore = parseInt(movie.Metascore);
+  const imdbRating = parseFloat(movie.imdbRating);
+  const imdbVotes = parseInt(movie.imdbVotes.replace(/,/g, ''));
+
+  let awardWins = 0;
+  let awardNominations = 0;
+  let oscarWins = 0;
+  let oscarNominations = 0;
+  let categories = movie.Awards.split('.');
+  let oscarString, otherString;
+  if (categories.length > 2) {
+    oscarString = categories[0];
+    otherString = categories[1];
+  } else {
+    oscarString = '';
+    otherString = categories[0];
+  }
+  let otherAwardsAndNominations = otherString.split(' ').filter((word) => word == parseInt(word));
+  awardWins = parseInt(otherAwardsAndNominations[0]);
+  awardNominations = parseInt(otherAwardsAndNominations[1]);
+  let oscarWinsOrNominations = parseInt(
+    oscarString.split(' ').filter((word) => word == parseInt(word))[0]
+  );
+  if (oscarString.includes('Nominated')) {
+    oscarNominations = oscarWinsOrNominations;
+  }
+  if (oscarString.includes('Won')) {
+    oscarWins = oscarWinsOrNominations;
+  }
+  let awardScore = 5 * oscarWins + oscarNominations + awardWins + 0.25 * awardNominations;
+  return {
+    awardScore: parseFloat(awardScore),
+    boxOfficeDollars: parseInt(boxOfficeDollars),
+    metascore: parseInt(metascore),
+    imdbRating: parseFloat(imdbRating),
+    imdbVotes: parseInt(imdbVotes),
+  };
+};
+
+const compareMovies = () => {
+  const leftSummaryArticles = document.querySelectorAll('#left-summary .notification');
+  const rightSummaryArticles = document.querySelectorAll('#right-summary .notification');
+
+  leftSummaryArticles.forEach((leftArticle, index) => {
+    const rightArticle = rightSummaryArticles[index];
+    if (parseFloat(leftArticle.dataset.value) > parseFloat(rightArticle.dataset.value)) {
+      leftArticle.classList.remove('is-primary', 'is-warning', 'is-success', 'is-danger');
+      leftArticle.classList.add('is-success');
+      rightArticle.classList.remove('is-primary', 'is-warning', 'is-success', 'is-danger');
+      rightArticle.classList.add('is-danger');
+    } else if (parseFloat(leftArticle.dataset.value) < parseFloat(rightArticle.dataset.value)) {
+      leftArticle.classList.remove('is-primary', 'is-warning', 'is-success', 'is-danger');
+      leftArticle.classList.add('is-danger');
+      rightArticle.classList.remove('is-primary', 'is-warning', 'is-success', 'is-danger');
+      rightArticle.classList.add('is-success');
+    } else {
+      leftArticle.classList.remove('is-primary', 'is-warning', 'is-success', 'is-danger');
+      leftArticle.classList.add('is-warning');
+      rightArticle.classList.remove('is-primary', 'is-warning', 'is-success', 'is-danger');
+      rightArticle.classList.add('is-warning');
+    }
+  });
 };
